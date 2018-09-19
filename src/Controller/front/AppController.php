74a8e6b102761587d2262cc438a56c7a2ea6ebaf
Nicolas SARRAZIN <nicolas.sarrazin@Hitema.peda>
2018-09-19 09:49:12 +0200

    front

--- src/Controller/front/AppController.php
+++ src/Controller/front/AppController.php
@@ -4,16 +4,23 @@ namespace App\Controller\front;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class AppController extends AbstractController
 {
-    /**
-     * @Route("/app", name="app")
-     */
     public function index()
     {
         return $this->render('app/index.html.twig', [
             'controller_name' => 'AppController',
+        ]);
+    }
+
+    public function map(Request $request): Response
+    {
+        return $this->render('map.html.twig', [
+            //'name' => $request->get('name', 'World')
         ]);
     }
 }
